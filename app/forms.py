6e8a27346e43f0a4e29d55ebdabff0b8248aaa67
dekinhitsuji <yattusyuiti@gmail.com>
2021-09-10 19:50:02 +0900

    9/10

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 #モデルクラスを呼出
 from .models import People
+from django.core.exceptions import ValidationError
+from django import forms
+from django.contrib.admin import widgets
+import os
 
+
+widget_textinput = forms.TextInput(
+    attrs={
+        "class":"form-control",
+    }
+)
 
 #フォームクラス作成
 class Contact_Form(forms.ModelForm):
@@ -16,10 +26,31 @@ class Contact_Form(forms.ModelForm):
 
         #③表示ラベルを定義
         labels = {
-            'Name':"名前",
+            'Name':"名前　　",
             'Furigana':"フリガナ",
             'Tell':"電話番号",
-            'Mail':"メール",
+            'Mail':"メール　",
             'Birthday':"生年月日",
-            'Adress':"住所",
+            'Adress':"住所　　",
         }
+        widget = {
+            'Name': widget_textinput,
+            'Furigana': widget_textinput,
+            'Tell': widget_textinput,
+            'Mail': widget_textinput,
+            'Birthday': widget_textinput,
+            'Adress': widget_textinput,
+        }
+
+
+
+class CheckeForm(forms.Form):
+    selected_time = forms.fields.ChoiceField(
+        choices = (
+            ('','同意します'),
+        ),
+        label = '上記の内容に同意できる場合下記ボックスにチェックを入れてください',
+        required = True,
+        widget = forms.widgets.RadioSelect
+
+    )
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'app'
+
 urlpatterns = [
-    path('formpage/', views.TopFormView.as_view(),name="formpage"),
-    path('doui/', views.Index.as_view(), name="doui"),
+    path('formpage/', views.FormView.as_view(),name="formpage"),
+    path('doui/', views.ChoiceView.as_view(), name="doui"),
 ]
